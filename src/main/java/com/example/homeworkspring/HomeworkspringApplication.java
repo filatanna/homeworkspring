db517025cd47624ad399854de86ac44fc8a9cd9d
User <filataanna@mail.ru>
2023-05-08 13:08:26 +0300

    первый коммит

---
+++ src/main/java/com/example/homeworkspring/HomeworkspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkspringApplication.class, args);
+    }
+
+}
